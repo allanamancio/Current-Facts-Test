74320f62d881531a5c3755e8fb0f8916b92baee6
allanamancio <allanamancio.rocha@gmail.com>
2018-11-22 11:27:12 -0200

    Initializing the test

---
+++ test.js
@@ -0,0 +1,9 @@
+"use strict";
+
+function currentFacts(facts, schema) {
+	// inputs: two arrays called "facts" and "schema".
+	// facts = [entitiy, attribute, value, boolean about "added?"]
+	// schema = [attribute, cardinality]
+	// output: one array with the 'current facts' from the "facts" array.
+	
+}
